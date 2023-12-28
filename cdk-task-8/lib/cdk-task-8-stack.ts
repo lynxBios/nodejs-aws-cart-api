6cfa7ee4dae60975c3d42f74e84cdd5cef85aaa1
lynxBios <123711868+lynxBios@users.noreply.github.com>
2023-12-28 18:33:04 +0300

    feat: wrap nest.js application to aws lambda

--- cdk-task-8/lib/cdk-task-8-stack.ts
+++ cdk-task-8/lib/cdk-task-8-stack.ts
@@ -1,16 +1,62 @@
 import * as cdk from 'aws-cdk-lib';
+import * as dotenv from 'dotenv';
+import * as apiGateway from '@aws-cdk/aws-apigatewayv2-alpha';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import 'source-map-support/register';
+
 
 export class CdkTask8Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const environment = {};
+    dotenv.config({ processEnv: environment });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkTask8Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cartService = new NodejsFunction(this, 'CartServiceLambda', {
+      runtime: Runtime.NODEJS_18_X,
+      functionName: 'cartService',
+      entry: '../dist/main.js',
+      environment,
+      bundling: {
+        externalModules: [
+          'mysql',
+          'mysql2',
+          'pg-query-stream',
+          'better-sqlite3',
+          'sqlite3',
+          'tedious',
+          'better-sqlite3',
+          'oracledb',
+          '@nestjs/microservices',
+          '@nestjs/microservices/microservices-module',
+          '@nestjs/websockets/socket-module',
+        ],
+      },
+    });
+    
+    const api = new apiGateway.HttpApi(this, 'CartApiGateway', {
+      corsPreflight: {
+        allowHeaders: ['*'],
+        allowOrigins: ['*'],
+        allowMethods: [apiGateway.CorsHttpMethod.ANY],
+      },
+    });
+
+    api.addRoutes({
+      path: '/{api+}',
+      methods: [apiGateway.HttpMethod.ANY],
+      integration: new HttpLambdaIntegration(
+        'CartServiceLambdaIntegration',
+        cartService,
+      ),
+    });
+
+    new cdk.CfnOutput(this, 'ApiUrl', {
+      value: api.url || '',
+    });
+
   }
 }
